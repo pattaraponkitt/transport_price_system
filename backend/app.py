9163756ffd5e25936112fb1b9986e5cc2e7e0cc5
pattarapon kittipanchasap <pattarapon.kitt@hotmail.com>
2024-04-24 23:35:41 +0700

    commit

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,65 @@ db = mysql.connector.connect(
 )
 cursor = db.cursor()
 
+def calculate_can_use_rate(mean_price, low_price, cal_price):
+    if mean_price < low_price:
+        return "{:,.2f} THB (USE : {:,.2f} THB)".format(mean_price, low_price)
+    elif mean_price >= low_price and mean_price <= cal_price:
+        return "{:,.2f} THB".format(mean_price)
+    else:
+        return "Not Applicable"
+
+def calculate_mean_price(standard_prices, low_prices):
+    mean_prices = []
+    for standard_price, low_price in zip(standard_prices, low_prices):
+        if standard_price < low_price:
+            mean_price = standard_price
+        else:
+            mean_price = (standard_price + low_price) / 2
+        mean_prices.append(mean_price)
+    return mean_prices
+
+def calculate_base_price(cursor, distance):
+    cursor.execute("SELECT price FROM price_tiers WHERE distance <= %s ORDER BY distance DESC LIMIT 1", (distance,))
+    basePrice_result = cursor.fetchone()
+    if basePrice_result:
+        basePrice = basePrice_result[0]
+    else:
+        cursor.execute("SELECT max_distance FROM fix_cost")
+        maxDistance = cursor.fetchone()[0]
+        if distance > maxDistance:
+            distance = maxDistance
+        cursor.execute("SELECT price FROM price_tiers WHERE distance = %s", (distance,))
+        basePrice = cursor.fetchone()[0]
+    return basePrice * distance
+
+def calculate_petro_price(cursor, petro):
+    cursor.execute("SELECT price FROM petro_tiers WHERE %s BETWEEN range_start AND range_end", (petro,))
+    petroPrice_result = cursor.fetchone()
+    if petroPrice_result:
+        petroPrice = petroPrice_result[0]
+    else:
+        cursor.execute("SELECT max_petro_tire FROM fix_cost")
+        petroPrice = cursor.fetchone()[0]
+    return petroPrice
+
+def calculate_standard_price(cursor, distance, petro_price, day):
+    cursor.execute("SELECT * FROM fix_cost")
+    fix_cost_data = cursor.fetchone()
+    yardCost = fix_cost_data[1]
+    portCost = fix_cost_data[2]
+    factoryCost = fix_cost_data[3]
+    perDayCost = fix_cost_data[4]
+
+    standard_price = (
+        calculate_base_price(cursor, distance) * petro_price +
+        yardCost +
+        portCost +
+        factoryCost +
+        ((day - 1) * perDayCost)
+    )
+    return standard_price
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -72,6 +131,8 @@ def home():
             low_prices.append(round(cal_price_range, -2))
         mean_prices = calculate_mean_price(standard_prices, low_prices)
 
+        can_use_rate = calculate_can_use_rate(mean_price, low_price, cal_price)
+
         cursor.execute("INSERT INTO transport_data (factory, distance, petro_price, day, price) VALUES (%s, %s, %s, %s, %s)", (factory, distance, petroPriceToday, day, cal_price))
         db.commit()
 
@@ -88,7 +149,8 @@ def home():
             low_price=round(low_price, -2),
             cal_price=round(cal_price, -2),
             mean_price=round(mean_price, -2),
-            petro_tiers_data=petro_tiers_data
+            petro_tiers_data=petro_tiers_data,
+            can_use_rate=can_use_rate
         )
     else:
         return render_template('index.html')
@@ -107,57 +169,6 @@ def history():
         cal_price_list.append(cal_price)
 
     return render_template('history.html', transport_data=data, cal_price_list=cal_price_list)
-
-def calculate_mean_price(standard_prices, low_prices):
-    mean_prices = []
-    for standard_price, low_price in zip(standard_prices, low_prices):
-        if standard_price < low_price:
-            mean_price = standard_price
-        else:
-            mean_price = (standard_price + low_price) / 2
-        mean_prices.append(mean_price)
-    return mean_prices
-
-def calculate_base_price(cursor, distance):
-    cursor.execute("SELECT price FROM price_tiers WHERE distance <= %s ORDER BY distance DESC LIMIT 1", (distance,))
-    basePrice_result = cursor.fetchone()
-    if basePrice_result:
-        basePrice = basePrice_result[0]
-    else:
-        cursor.execute("SELECT max_distance FROM fix_cost")
-        maxDistance = cursor.fetchone()[0]
-        if distance > maxDistance:
-            distance = maxDistance
-        cursor.execute("SELECT price FROM price_tiers WHERE distance = %s", (distance,))
-        basePrice = cursor.fetchone()[0]
-    return basePrice * distance
-
-def calculate_petro_price(cursor, petro):
-    cursor.execute("SELECT price FROM petro_tiers WHERE %s BETWEEN range_start AND range_end", (petro,))
-    petroPrice_result = cursor.fetchone()
-    if petroPrice_result:
-        petroPrice = petroPrice_result[0]
-    else:
-        cursor.execute("SELECT max_petro_tire FROM fix_cost")
-        petroPrice = cursor.fetchone()[0]
-    return petroPrice
-
-def calculate_standard_price(cursor, distance, petro_price, day):
-    cursor.execute("SELECT * FROM fix_cost")
-    fix_cost_data = cursor.fetchone()
-    yardCost = fix_cost_data[1]
-    portCost = fix_cost_data[2]
-    factoryCost = fix_cost_data[3]
-    perDayCost = fix_cost_data[4]
-
-    standard_price = (
-        calculate_base_price(cursor, distance) * petro_price +
-        yardCost +
-        portCost +
-        factoryCost +
-        ((day - 1) * perDayCost)
-    )
-    return standard_price
 
 if __name__ == '__main__':
     app.run(debug=True)
